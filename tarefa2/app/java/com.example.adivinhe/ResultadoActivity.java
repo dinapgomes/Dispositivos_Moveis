1734d0ee850fab9e764c57463366878b821d8ea3
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-06-13 19:45:38 -0300

    Adiciona imports

--- tarefa2/app/java/com.example.adivinhe/ResultadoActivity.java
+++ tarefa2/app/java/com.example.adivinhe/ResultadoActivity.java
@@ -1,3 +1,13 @@
+package com.example.adivinhacao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public class ResultadoActivity extends AppCompatActivity {
     private Button homeButton;
 
